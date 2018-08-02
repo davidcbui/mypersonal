c5d7c2942ab2df92443de00a0d5c3fd77937d707
David Bui <dbui@dmrs.net>
2018-08-02 16:22:12 -0500

    added resume

--- app/home/views.py
+++ app/home/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, send_from_directory
 
 from flask_mail import Mail, Message
 
@@ -6,6 +6,7 @@ from flask_mail import Mail, Message
 
 # import simplejson as json
 from . import home
+import webbrowser as wb
 
 mail = Mail()
 
@@ -31,4 +32,14 @@ def contact_form():
 
     return jsonify(
         success=1, status="OK", name=name, email=email, subject=subject, message=message
+    )
+
+
+@home.route("/viewcv")
+def get_cv():
+    mycv = "resume2018.pdf"
+    directory = "static"
+    # wb.open_new(mycv)
+    return send_from_directory(
+        directory=directory, filename=mycv, mimetype="application/pdf"
     )
