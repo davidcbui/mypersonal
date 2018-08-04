f3fab6c7fb6289c0c9afce3db24988a0bf76e534
David Bui <dbui@dmrs.net>
2018-08-04 10:21:04 -0500

    more trimming code

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,9 +3,10 @@ import unittest
 from app import create_app, db
 from app.models import User, Role
 
+
 class FlaskClientTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = create_app('testing')
+        self.app = create_app("testing")
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
@@ -18,45 +19,54 @@ class FlaskClientTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_home_page(self):
-        response = self.client.get('/')
+        response = self.client.get("/welcome")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('Stranger' in response.get_data(as_text=True))
+        self.assertTrue("Stranger" in response.get_data(as_text=True))
 
     def test_register_and_login(self):
         # register a new account
-        response = self.client.post('/auth/register', data={
-            'email': 'john@example.com',
-            'username': 'john',
-            'password': 'cat',
-            'password2': 'cat'
-        })
+        response = self.client.post(
+            "/auth/register",
+            data={
+                "email": "john@example.com",
+                "username": "john",
+                "password": "cat",
+                "password2": "cat",
+            },
+        )
         self.assertEqual(response.status_code, 302)
 
         # login with the new account
-        response = self.client.post('/auth/login', data={
-            'email': 'john@example.com',
-            'password': 'cat'
-        }, follow_redirects=True)
+        response = self.client.post(
+            "/auth/login",
+            data={"email": "john@example.com", "password": "cat"},
+            follow_redirects=True,
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(re.search('Hello,\s+john!',
-                                  response.get_data(as_text=True)))
+        self.assertTrue(re.search("Hello,\s+john!", response.get_data(as_text=True)))
         self.assertTrue(
-            'You have not confirmed your account yet' in response.get_data(
-                as_text=True))
+            "You have not confirmed your account yet" in response.get_data(as_text=True)
+        )
 
         # send a confirmation token
-        user = User.query.filter_by(email='john@example.com').first()
+        user = User.query.filter_by(email="john@example.com").first()
         token = user.generate_confirmation_token()
-        response = self.client.get('/auth/confirm/{}'.format(token),
-                                   follow_redirects=True)
+        response = self.client.get(
+            "/auth/confirm/{}".format(token), follow_redirects=True
+        )
         user.confirm(token)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(
-            'You have confirmed your account' in response.get_data(
-                as_text=True))
+            "You have confirmed your account" in response.get_data(as_text=True)
+        )
 
         # log out
-        response = self.client.get('/auth/logout', follow_redirects=True)
+        response = self.client.get("/auth/logout", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('You have been logged out' in response.get_data(
-            as_text=True))
+        self.assertTrue("You have been logged out" in response.get_data(as_text=True))
+
+    def test_home_about(self):
+        response = self.client.get("/about")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("David Bui" in response.get_data(as_text=True))
+
